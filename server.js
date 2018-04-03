7d90bb6583df7ef12b80678ff1e90c341841d70f
Fred Luartes <fjluartes@gmail.com>
2018-04-03 10:07:19 +0800

    Added index.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var http = require("http");
-function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {"Content-type": "text/plain"});
-    response.write("Hello World!");
-    response.end();
+
+function start() {
+    function onRequest(request, response) {
+        console.log("Request received.");
+        response.writeHead(200, {"Content-type": "text/plain"});
+        response.write("Hello World!");
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
 }
-http.createServer(onRequest).listen(8888);
-console.log("Server has started.");
+
+exports.start = start;
