180499c11722caf5cf96b6a202bed31c954df2b3
Fred Luartes <fjluartes@gmail.com>
2018-04-03 10:27:08 +0800

    edited server, index; added router

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
-        console.log("Request received.");
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        route(pathname);
+
         response.writeHead(200, {"Content-type": "text/plain"});
         response.write("Hello World!");
         response.end();
