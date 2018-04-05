240c3c83dd32067888a96dc5c092ac36afaee066
Fred Luartes <fjluartes@gmail.com>
2018-04-05 14:49:24 +0800

    reviewed server and router

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 function route(pathname) {
-    console.log("About to route a request for " + pathname);
+    console.log("About to route a request to " + pathname);
 }
 
-exports.route = route;
+exports.route = route; 
--- server.js
+++ server.js
@@ -4,10 +4,9 @@ var url = require("url");
 function start(route) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
+        console.log("Request to " + pathname + " received.");
         route(pathname);
-
-        response.writeHead(200, {"Content-type": "text/plain"});
+        response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World!");
         response.end();
     }
