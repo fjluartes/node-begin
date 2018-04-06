13fc758d0634d7828c0ddc18983be0216167d451
Fred Luartes <fjluartes@gmail.com>
2018-04-06 11:24:04 +0800

    added querystring to parse POST request

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,4 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
@@ -24,7 +24,8 @@ function start(response, postData) {
 function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent: " + postData);
+    response.write("You've sent the text: " + 
+        querystring.parse(postData).text);
     response.end();
 }
 
