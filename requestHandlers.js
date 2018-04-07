e83d09993258e4e6bd2540de5c599141ce6af52b
Fred Luartes <fjluartes@gmail.com>
2018-04-07 11:53:18 +0800

    review of start and upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,19 +2,18 @@ var querystring = require("querystring");
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
-    
     var body = '<html>' +
-        '<head>' + 
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
-        '</head>' +
-        '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
-        '</form>' +
-        '</body>' + 
-        '</html>';
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ function start(route, handle) {
     function onRequest(request, response) {
         var postData = "";
         var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
+        console.log("Request received.");
         request.setEncoding("utf8");
 
         request.addListener("data", function(postDataChunk) {
